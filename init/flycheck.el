475ad1a33ef9fe3f11257560aee49f1beef1e927
jilen <jilen.zhang@gmail.com>
2013-03-23 00:07:21 +0800

    switch auto-complete to master

--- init/flycheck.el
+++ init/flycheck.el
@@ -1,6 +1,4 @@
-;;flycheck mode
 (require 'flycheck)
-;;remove elisp doc checker
 (eval-after-load 'flycheck '(setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers)))
 (add-hook 'prog-mode-hook 'flycheck-mode)
 (add-hook 'text-mode-hook 'flycheck-mode)
--- modules.el
+++ modules.el
@@ -12,30 +12,39 @@
   (concat "~/.emacs.d/init/"
 	  (concat module ".el")))
 
-(defun add-module-path (elisp)
-  (add-to-list 'load-path (module-dir elisp)))
+(defun add-module-path (module)
+  (cond(
+	(file-exists-p (module-dir module))
+	(add-to-list 'load-path (module-dir module))))
+  (cond(
+	(file-exists-p (module-dir (concat module "/elisp")))
+	(add-to-list 'load-path (module-dir(concat module "/elisp"))))))
+
 (defun init-module (module)
-  (load-file (module-init-file module)))
+  (cond
+   (
+    (file-exists-p (module-init-file module))
+    (load-file (module-init-file module)))))
 
 ;;;add module to load path, and load init file
 (defun load-module (module) 
   (add-module-path module)
   (init-module module))
 
+;;utility library
+(load-module "popup")
+(load-module "fuzzy")
+(load-module "s")
+(load-module "dash")
 
+;;modes
 (load-module "yasnippet")
 (load-module "auto-complete")
 (load-module "php-mode")
-(load-module "s")
-(load-module "dash")
 (load-module "flycheck")
 (load-module "haskell-mode")
 (load-module "scala-mode2")
-
-;;ensime's el files are located in a sub directory
-(add-module-path "ensime/elisp")
-(init-module "ensime")
-
+(load-module "ensime")
 
 (provide 'modules)
 ;;; modules.el ends here
