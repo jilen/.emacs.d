bbfdc4c27b020fd3a555048cbc1d0dddabf66ba2
jilen <jilen.zhang@gmail.com>
2013-03-24 00:35:07 +0800

    auto start ac anytime except eval buffer

--- init/auto-complete.el
+++ init/auto-complete.el
@@ -1,3 +1,9 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/modules/yasnippet/dict")
 (ac-config-default)
+(define-globalized-minor-mode global-auto-complete-mode
+  auto-complete-mode (lambda ()
+                       (if (not (minibufferp (current-buffer)))
+			   (auto-complete-mode 1))
+                       ))
+(global-auto-complete-mode t)
