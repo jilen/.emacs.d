24ee28e2ff6c083f71935f8b85b9b12e4f2c42fc
jilen <jilen.zhang@gmail.com>
2023-03-07 09:25:40 +0800

    better lsp frame style

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -26,7 +26,49 @@
                   (replace-match ""))
                 (apply orig rest))))
 
+
 (use-package "lsp-ui"
+  :config
+  (defun make-doc-frame ()
+    "Create the child frame and return it."
+    (lsp-ui-doc--delete-frame)
+    (let* ((after-make-frame-functions nil)
+           (before-make-frame-hook nil)
+           (name-buffer (lsp-ui-doc--make-buffer-name))
+           (buffer (get-buffer name-buffer))
+           (params (append lsp-ui-doc-frame-parameters
+                           `((name . "")
+                             (child-frame-border-width . 1)
+                             (default-minibuffer-frame . ,(selected-frame))
+                             (minibuffer . ,(minibuffer-window))
+                             (left-fringe . 1)
+                             (right-fringe . 1)
+                             (cursor-type . nil)
+                             (lsp-ui-doc--no-focus . t))))
+           (window (display-buffer-in-child-frame
+                    buffer
+                    `((child-frame-parameters . ,params))))
+           (frame (window-frame window)))
+      (with-current-buffer buffer
+        (lsp-ui-doc-frame-mode 1))
+      (set-frame-parameter nil 'lsp-ui-doc-buffer buffer)
+      (set-window-dedicated-p window t)
+      (run-hook-with-args 'lsp-ui-doc-frame-hook frame window)
+      (when lsp-ui-doc-use-webkit
+        (define-key (current-global-map) [xwidget-event]
+                    (lambda ()
+                      (interactive)
+                      (let ((xwidget-event-type (nth 1 last-input-event)))
+                        ;; (when (eq xwidget-event-type 'load-changed)
+                        ;;   (lsp-ui-doc--move-frame (lsp-ui-doc--get-frame)))
+                        (when (eq xwidget-event-type 'javascript-callback)
+                          (let ((proc (nth 3 last-input-event))
+                                (arg (nth 4 last-input-event)))
+                            (funcall proc arg))))))
+        (lsp-ui-doc--webkit-run-xwidget))
+      frame)
+    )
+  (advice-add 'lsp-ui-doc--make-frame :override #'make-doc-frame)
   :init
   (setq lsp-ui-sideline-enable nil)
   (setq lsp-ui-doc-enable t)
@@ -34,7 +76,9 @@
   (setq lsp-enable-indentation nil)
   (setq lsp-ui-doc-show-with-cursor t)
   (setq lsp-ui-doc-delay 0.5)
+  (setq lsp-ui-doc-use-childframe t)
   (setq lsp-ui-doc-position 'at-point)
+  (setq )
   )
 
 (provide 'init-lsp)
