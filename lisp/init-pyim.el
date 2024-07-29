aa359f738e0267f0c34fff02fb2d68cc5b86808e
jilen <jilen.zhang@gmail.com>
2024-07-29 12:34:46 +0800

    fix base-dict init order

--- lisp/init-pyim.el
+++ lisp/init-pyim.el
@@ -10,13 +10,13 @@
   (setq pyim-page-tooltip 'posframe)
   (require 'pyim-liberime)
   (setq pyim-default-scheme 'rime-quanpin)
-  (require 'pyim-basedict) ; 拼音词库设置，五笔用户 *不需要* 此行设置`
-  (pyim-basedict-enable)   ; 拼音词库，五笔用户 *不需要* 此行设置
   (global-set-key (kbd "C-\\") 'toggle-input-method)
   (setq default-input-method "pyim"))
 
-(use-package pyim-basedict
-  :config
+(use-package pyim-basedict)
+
+(with-eval-after-load "pyim-basedict"
+  (require 'pyim-basedict) ; 拼音词库设置，五笔用户 *不需要* 此行设置
   (pyim-basedict-enable))
 
 
