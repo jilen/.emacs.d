c3e7714d9dac22218dbe71c44da3f48cdb9949ca
jilen <jilen.zhang@gmail.com>
2025-03-07 20:04:12 +0800

    增加ai助手

---
+++ lisp/init-assist.el
@@ -0,0 +1,36 @@
+;;; init-assist.el --- Codeium AI assist
+
+;;; Commentary:
+;;
+
+
+(use-package codeium
+  :load-path "~/.emacs.d/site-lisp/codeium/"
+
+  :init
+  ;; use globally
+  (add-to-list 'completion-at-point-functions #'codeium-completion-at-point)
+  :config
+  (setq codeium/metadata/api_key (getenv "CODEIUM_API_KEY"))
+  (setq use-dialog-box nil) ;; do not use popup boxes
+
+  ;; use M-x codeium-diagnose to see apis/fields that would be sent to the local language server
+  (setq codeium-api-enabled
+        (lambda (api)
+          (memq api '(GetCompletions Heartbeat CancelRequest GetAuthToken RegisterUser auth-redirect AcceptCompletion)))))
+
+
+(use-package aidermacs
+  :load-path "~/.emacs.d/site-lisp/aidermacs"
+  :bind (("C-c a" . aidermacs-transient-menu))
+
+  :config
+  (aidermacs-setup-minor-mode)
+  :custom
+  (aidermacs-auto-commits t)
+  (aidermacs-use-architect-mode t)
+  (aidermacs-default-model "sonnet")
+  )
+
+(provide 'init-assist)
+;;; init-assist.el ends here
