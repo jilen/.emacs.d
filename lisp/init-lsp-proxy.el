40a79e97ebd364f0bfc13ee49fe8f68655be58bd
jilen <jilen.zhang@gmail.com>
2025-10-13 10:33:50 +0800

    Don't nedd handle forward

--- lisp/init-lsp-proxy.el
+++ lisp/init-lsp-proxy.el
@@ -21,33 +21,6 @@
 
 (require 'lsp-proxy)  ; Ensure loaded
 
-(defun lsp-proxy--handle-vue-tsserver-request (orig-fun method &rest args)
-  "Intercept tsserver/request and forward via execute-command if possible."
-  (if (eq method 'tsserver/request)
-      (let* ((msg (car args))
-             (id (nth 0 msg))
-             (command (nth 1 msg))
-             (payload (nth 2 msg)))
-        ;; Forward using execute-command to current LSP process
-        (lsp-proxy--execute-command
-         "typescript.tsserverRequest"
-         (list :command command :arguments payload)
-         ;; No server ID — just use default connection
-         "ts_ls"
-         :success-fn (lambda (response)
-                       (lsp-proxy--notify
-                        'tsserver/response
-                        (list (list id (plist-get response :body)))))
-         :error-fn (lambda (_err)
-                     (lsp-proxy--notify
-                      'tsserver/response
-                      (list (list id nil))))))
-    ;; Not our message — pass through
-    (apply orig-fun method args)))
-
-(advice-add 'lsp-proxy--handle-notification :around #'lsp-proxy--handle-vue-tsserver-request)
-
-
 (provide 'init-lsp-proxy)
 
 ;;; init-lsp-proxy.el ends here
